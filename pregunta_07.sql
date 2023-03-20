214d3c6c8644f79ed812bca0d7a709b2bf7730fe
tbetancur <tbetancur@unal.edu.co>
2023-03-19 22:50:22 -0500

    Solucion
    Solucion 3er taller

--- pregunta_07.sql
+++ pregunta_07.sql
@@ -42,4 +42,4 @@
 --  12  C  24  482.32  300  2019-05-03  0.11  CCCE
 -- 
 --  >>> Escriba su codigo a partir de este punto <<<
---
+SELECT * FROM tbl1 WHERE K0 NOT IN ('A', 'B') AND C13 NOT IN ('200', '900') ORDER BY c14
